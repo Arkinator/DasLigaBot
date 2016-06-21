7df09d2ceac8cd39a4e689f001d541ff833ca689
azthec <azthec@users.noreply.github.com>
2016-06-21 12:58:50 +0100

    Create LineupRepository.java

---
+++ src/main/java/com/planed/ctlBot/domain/LineupRepository.java
@@ -0,0 +1,64 @@
+package com.planed.ctlBot.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.StreamUtils;
+import org.springframework.stereotype.Repository;
+
+import com.planed.ctlBot.data.LineupEntity;
+import com.planed.ctlBot.data.repositories.LineupEntityRepository;
+
+/**
+ * 
+ * @author azthec
+ *
+ */
+@Repository
+public class LineupRepository { //couple of functions and how to save object from memory to db
+	    private final Mapper mapper = new DozerBeanMapper();
+	    @Autowired
+	    private LineupEntityRepository lineupEntityRepository;
+	    
+	    
+	    public List<Lineup> findAll() {
+	        final List<Lineup> result = new ArrayList<Lineup>();
+	        StreamUtils.createStreamFromIterator(lineupEntityRepository.findAll().iterator())
+	                .map(o -> mapFromEntity(o))
+	                .forEach(result::add);
+	        return result;
+	    }
+
+	    public Lineup findByLineupId(final String lineupId) {
+	        final LineupEntity lineupEntity = lineupEntityRepository.findOne(lineupId);
+	        if (lineupEntity == null) {
+	            return null;
+	        }
+	        return mapFromEntity(lineupEntity);
+	    }
+
+	    public void save(final Lineup... lineups) {
+	        for (final Lineup lineup : lineups) {
+	            final LineupEntity lineupEntity = mapToEntity(lineup);
+	            lineupEntityRepository.save(lineupEntity);
+	        }
+	    }
+
+	    private LineupEntity mapToEntity(final Lineup lineup) {
+	        final LineupEntity result = mapper.map(lineup, LineupEntity.class);
+	        result.setLineupId(lineup.getLineupId());
+	        return result;
+	    }
+
+	    private Lineup mapFromEntity(final LineupEntity lineup) {
+	        final Lineup result = mapper.map(lineup, Lineup.class);
+	        return result;
+	    }
+
+	    public Lineup refresh(final Lineup lineup) {
+	        return mapFromEntity(lineupEntityRepository.findOne(lineup.getLineupId()));
+	    }
+}
